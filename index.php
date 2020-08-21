6ea7100d4e8811d357e8f308e1e8de21849397f3
Wilian Liz <wliz2726@gmail.com>
2020-08-20 22:04:35 -0400

    .

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 if (isset($_POST['submit'])) {
     //ini_set( 'display_errors', 1 ); # REMOVE // FOR DEBUG
     //error_reporting( E_ALL ); # REMOVE // FOR DEBUG
-    $from = "wliz2726@gmail.com"; // Email con el dominio del Hosting para evitar SPAM
-    $fromName = "RPF-WEB"; // Nombre que saldrá en el email recibido
-    $to = "wliz@encorerepair.com"; // Dirección donde se enviará el formulario
-    $subject = "Solicitud de Asistencia"; // Asunto del Formulario
+    $from = "info@westore.do"; // Email con el dominio del Hosting para evitar SPAM
+    $fromName = "Webstore"; // Nombre que saldrá en el email recibido
+    $to = "bduran@westore.do"; // Dirección donde se enviará el formulario
+    $subject = "Solicitud de Cliente"; // Asunto del Formulario
 
     /* Componemos el mensaje */
     $fullMessage = "Hola! " . $to . "\r\n";
@@ -14,13 +14,14 @@ if (isset($_POST['submit'])) {
     $fullMessage .= "Nombre: " . $_POST['validarNombre'] . "\r\n";
     $fullMessage .= "E-Mail: " . $_POST['validarEmail'] . "\r\n";
     $fullMessage .= "Teléfono: " . $_POST['validarTelefono'] . "\r\n";
-    $fullMessage .= "Asunto: " . "Solicitud de Asistencia" . "\r\n";
+    $fullMessage .= "Asunto: " . "Solicitud de Cliente" . "\r\n";
     $fullMessage .= "Mensaje: " . $_POST['validationMensaje'] . "\r\n";
+    $fullMessage .= "Enviado el " . date('d/m/Y', time());
     $fullMessage .= "\r\n";
     $fullMessage .= "Saludos!\r\n";
 
     /* Creamos las cabeceras del Email */
-    $headers = "Organization: RPF WEB\r\n";
+    $headers = "Organization: Webstore\r\n";
     $headers .= "From: " . $fromName . "<" . $from . ">\r\n";
     $headers .= "Reply-To: " . $_POST['validarEmail'] . "\r\n";
     $headers .= "Return-Path: " . $to . "\r\n";
@@ -29,11 +30,8 @@ if (isset($_POST['submit'])) {
     $headers .= "X-Priority: 3\r\n";
     $headers .= "X-Mailer: PHP". phpversion() ."\r\n";
 
-    /* Enviamos el Formulario*/
-    if (mail($to, $subject, $fullMessage, $headers)) {
-        echo "<center><h2>El E-Mail se ha enviado correctamente!</h2></center>";
-    } else {
-        echo "<center><h2>Ops ! El E-Mail ha fallado!</h2></center>S";
-    }
+    header("Location:index.html");
+
+    
 }
 ?>
