4ddb11199d960ba01473ed72b380b170a3c5181b
Wilian Liz <wliz2726@gmail.com>
2020-09-04 16:42:21 -0400

    Detalles del precio

--- demo/marulaboutique/catalog/language/es-ES/account/wishlist.php
+++ demo/marulaboutique/catalog/language/es-ES/account/wishlist.php
@@ -1,12 +1,12 @@
 <?php
-$_['heading_title'] 	=  'Mi lista de deseos';
+$_['heading_title'] 	=  'Mis Favoritos';
 $_['text_account'] 	=  'Cuenta';
 $_['text_instock'] 	=  'En stock';
-$_['text_wishlist'] 	=  'Listas de deseos)';
-$_['text_login'] 	=  'Debe <a href="%s"> iniciar sesión </a> o <a href="%s"> crear una cuenta </a> para guardar <a href="%s"> %s </a> A su <a href="%s"> lista de deseos </a>!';
-$_['text_success'] 	=  'Éxito: ha añadido <a href="%s"> %s </a> a su <a href="%s"> lista de deseos </a>!';
-$_['text_remove'] 	=  'Éxito: ¡Has modificado tu lista de deseos!';
-$_['text_empty'] 	=  'Tu lista de deseos Esta vacía.';
+$_['text_wishlist'] 	=  'Favoritos)';
+$_['text_login'] 	=  'Debe <a href="%s"> iniciar sesión </a> o <a href="%s"> crear una cuenta </a> para guardar <a href="%s"> %s </a> en su <a href="%s"> Articulos Favoritos </a>!';
+$_['text_success'] 	=  'Listo: ha añadido <a href="%s"> %s </a> a su <a href="%s"> Lista de Favoritos </a>!';
+$_['text_remove'] 	=  'Listo: ¡Has modificado tu lista de Favoritos!';
+$_['text_empty'] 	=  'Tu lista de Favoritos Esta vacía.';
 $_['column_image'] 	=  'Imagen';
 $_['column_name'] 	=  'Nombre del producto';
 $_['column_model'] 	=  'Modelo';
